4ea4933d2e654800af5c8ba9ca9f3f68350465a7
Chris Manson <chris.manson@simplabs.com>
2018-11-16 11:20:29 +0000

    renaming loading test

--- tests/acceptance/substates-test.js
+++ tests/acceptance/substates-test.js
@@ -2,14 +2,14 @@ import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | loading', function(hooks) {
+module('Acceptance | substates', function(hooks) {
   setupApplicationTest(hooks);
 
   test('loading class is added correctly', async function(assert) {
     let promise = visit('/slow');
 
     await new Promise(function(resolve) {
-      setTimeout(() => resolve("done!"), 150);
+      setTimeout(() => resolve(), 150);
     });
 
     assert.ok(document.body.classList.contains('loading'), "loading class is missing");
